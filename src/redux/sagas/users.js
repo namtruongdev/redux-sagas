1fde47d9fc6222468421bee32d7b6f1449ac881b
DUONGNAMTRUONG <namtruongdev@gmail.com>
2020-11-05 17:35:56 +0700

    update

---
+++ src/redux/sagas/users.js
@@ -0,0 +1,31 @@
+import { call, put, take, takeEvery } from 'redux-saga/effects';
+
+const apiUrl = 'https://5fa3d0d9f10026001618df85.mockapi.io/users';
+
+const getApi = () => {
+  return fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      throw err;
+    });
+};
+
+function* fetchUsers(action) {
+  try {
+    const users = yield call(getApi);
+    yield put({ type: 'GET_USERS_SUCCESS', users: users });
+  } catch (e) {
+    yield put({ type: 'GET_USERS_FAILED', message: e.message });
+  }
+}
+
+function* userSaga() {
+  yield takeEvery('GET_USERS_REQUESTED', fetchUsers);
+}
+
+export default userSaga;
